182249c5ed6bab2bf9c76a8e32ac7174a4e61049
imamiya-masaki <imamiyagit@gmail.com>
2021-03-20 18:13:27 +0900

    疎通確認

--- loadTool.py
+++ loadTool.py
@@ -1,70 +1,70 @@
 from dotenv import load_dotenv
 import urllib.request, json
 import os
+import requests
 import base64
 load_dotenv()
-ALLOWED_EXTENSIONS = set(['png', 'jpg'])
+ALLOWED_EXTENSIONS = set(["png", "jpg"])
 
 def allowed_file(file):
     filename = file.filename
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return "." in filename and \
+        filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
 
 def helloWorld():
-    return 'Hello Wrold'
+    return "Hello Wrold"
 
 def simpleOCR(file):
     #とりあえず、一枚の画像をOCRに投げるために
     print(file)
-    data = base64.b64encode (file.read())
+    data = base64.b64encode(file.read()).decode('utf-8')
     to_datas = []
-    to_datas.append({'data': data, 'format': 'png', 'name': 'simple'})
+    to_datas.append({"data": data, "format": "png", "name": "simple"})
     res = postOCR(to_datas)
-    print(res)
-    return res
+    print(res, res.json())
+    return json.dumps(res.json()).encode("utf-8")
 
 def postOCR(getImages = []):
     #getImageは、{data(must),format(any), name(any)}を受け取る
     #getImage = [{},{}...]
-    url = os.getenv('APIGW_INVOKE_URL')
+    url = os.getenv("APIGW_INVOKE_URL")
     version = "V2"
     requestId = "sbwmRequestId!!"
-    timestamp = 0
-    lang = 'ja'
+    timestamp = "0"
+    lang = "ja"
     images = []
     uniqueNumber = 0
     for item in getImages:
         postImage = {}
-        postImage['format'] = 'png'
-        postImage['data'] = item['data']
-        if type(item) == 'str':
+        postImage["format"] = "png"
+        postImage["data"] = item["data"]
+        if type(item) == "str":
             #maybe only data?
-            postImage['data'] = item
-            postImage['name'] = str(uniqueNumber)
+            postImage["data"] = item
+            postImage["name"] = str(uniqueNumber)
             uniqueNumber += 1
         else:
-            postImage['data'] = item['data']
-            if 'format' in item and item['format'] == 'jpg':
-                postImage['format'] = 'jpg'
-            if 'name' in item:
-                postImage['name'] = item['name']
+            postImage["data"] = item["data"]
+            if "format" in item and item["format"] == "jpg":
+                postImage["format"] = "jpg"
+            if "name" in item:
+                postImage["name"] = item["name"]
             else:
-                postImage['name'] = str(uniqueNumber)
+                postImage["name"] = str(uniqueNumber)
                 uniqueNumber += 1
+        #postImage["data"] = "" # 仮
         images.append(postImage)
     obj = {}
-    obj['version'] = version
-    obj['requestId'] = requestId
-    obj['timestamp'] = timestamp
-    obj['lang'] = lang
-    obj['images'] = images
+    obj["version"] = version
+    obj["requestId"] = requestId
+    obj["timestamp"] = timestamp
+    obj["lang"] = lang
+    obj["images"] = images
     headers = {}
     method = "POST"
-    headers['Content-Type'] = 'application/json'
-    headers['X-OCR-SECRET'] = os.getenv('SECRET_KEY')
+    headers["Content-Type"] = "application/json"
+    headers["X-OCR-SECRET"] = os.getenv("SECRET_KEY")
+    print()
     json_data = json.dumps(obj).encode("utf-8")
-    request = urllib.request.Request(url, data=json_data, method=method, headers=headers)
-    with urllib.request.urlopen(request) as response:
-        response_body = response.read().decode("utf-8")
-        print('OCR:output', response_body)
-        return response_body
+    request = requests.post(url, data=json_data, headers=headers)
+    return request
--- localTest.py
+++ localTest.py
@@ -1,2 +1,9 @@
 from loadTool import *
+import os
+import cv2
 
+fileDir = './localImage/'
+fileName = 'IMG_2747.png'
+
+with open(fileDir+ fileName, "rb") as f:
+    simpleOCR(f)
