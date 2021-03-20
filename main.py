ff53c9ab1cfa5b17f764fe249c0f64e4a636b8d5
imamiya-masaki <imamiyagit@gmail.com>
2021-03-21 01:14:34 +0900

    ローカルに戻す

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
+  app.run(debug=True, port=5000)
 @app.route('/')
 def route():
     return helloWorld()
