7961f3bb0a0642bf0f523b56933f093ae779c290
imamiya-masaki <imamiyagit@gmail.com>
2021-03-20 22:33:38 +0900

    indent

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import *
 from loadTool import *
 from werkzeug.utils import secure_filename
 import base64
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,20 +11,22 @@ def route():
 
 @app.route('/load', methods=["GET", "POST"])
 def load():
-    output = {}
-    if request.method ==  'POST':
-        print('request', request)
-        if 'img_file' not in request.form:
-            output['status'] = "400"
-            output['error'] = 'img_file not in request'
- #       elif not allowed_file(request.form['img_file']):
+  output = {}
+  if request.method ==  'POST':
+    print('request', request)
+    if 'img_file' not in request.form:
+      output['status'] = "400"
+      output['error'] = 'img_file not in request'
+#       elif not allowed_file(request.form['img_file']):
 #         output['status'] = 400
- #           output['error'] = 'can use extension type'
-        else:
-            #success
-            #img_file = request.form['img_file']
-            #filename = secure_filename(img_file.filename)
-            img = base64.b64decode(request.form['img_file'])
-            print('args', request, img)
-            return  {'status': "200", 'data': simpleOCR(img)}
+#           output['error'] = 'can use extension type'
+    else:
+      #success
+      #img_file = request.form['img_file']
+      #filename = secure_filename(img_file.filename)
+      img = base64.b64decode(request.form['img_file'])
+      output['status'] = "200"
+      output['data'] = simpleOCR(img)
+      #print('args', request, img)
+    return  json.dumps({'status': "200", 'data': simpleOCR(img)})
 
